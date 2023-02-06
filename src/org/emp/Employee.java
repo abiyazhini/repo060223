4fca396fff643680cbac02a4f53f47136820d0e5
abiyazhini <vaanchinathan77@gmail.com>
2023-02-06 09:13:57 +0530

    task 060223

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,26 @@
+package org.emp;
+
+public class Employee {
+
+int a=10,b=11,c;
+
+	public void empId() {
+	System.out.println("Employee ID: 1000");
+	}
+	public void empName() {
+		System.out.println("Employee Name: Vanchi");
+	}
+	public void empDob() {
+		System.out.println("Employee DOB: 02.05.1987");
+	}
+	
+	public void empPhone() {
+		System.out.println("Employee Phone: 9500705770");
+	}
+	public void empEmail() {
+		System.out.println("Employee Email: abc@gmail.com");
+		c= a+100;
+	}
+	
+	
+	}
---
+++ src/org/emp/Employee2.java
@@ -0,0 +1,23 @@
+package org.emp;
+
+public class Employee2 {
+	int a=10,b=11,c;
+	public void empId() {
+	System.out.println("Employee ID: 1000");
+	}
+	public void empName() {
+		System.out.println("Employee Name: Vanchi");
+	}
+	public void empDob() {
+		System.out.println("Employee DOB: 02.05.1987");
+	}
+	
+	public void empPhone() {
+		System.out.println("Employee Phone: 9500705770");
+	}
+	public void empEmail() {
+		System.out.println("Employee : abc@gmail.com");
+		c=a+b;
+	}
+	
+}
